8d172a1b90ab6965dc747d617023f4fbcb319dce
Luiz Felipe Da Silveira <luiz.fsilveira@linx.com.br>
2023-01-04 18:09:23 -0300

    Adicionando a rota raiz e uma rota de erros

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Root } from "./routes/root";
+import { ErrorPage } from "./error-page";
+
+// 1 - A primeira coisa a se fazer é criar um browser router
+// 2 - E depois criar nossa primeira rota
+// 3 - Para criar uma rota é necessário:
+//  3.1 - Informar o caminho que será acessada no browser.
+//  3.2 - Informar o elemento/componente que será renderizado.
+// 4 - Passar o RouterProvider para nossa aplicação, para que ele forneça as rotas configuradas para todos os componentes filhos.
+
+// {1}
+const router = createBrowserRouter([
+  // {2}
+  {
+    // 3.1
+    path: "/",
+    // 3.2
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* {4} */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
